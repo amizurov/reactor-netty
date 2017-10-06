3d6d41fff283e423565edfc5530db85a2b8ac0a4
Stephane Maldini <smaldini@pivotal.io>
2017-10-06 15:03:23 -0700

    Rename onClose -> onDispose Add DisposableChannel super-interface

--- src/test/java/reactor/ipc/netty/http/server/HttpServerTests.java
+++ src/test/java/reactor/ipc/netty/http/server/HttpServerTests.java
@@ -173,7 +173,7 @@ public class HttpServerTests {
 				          .block(Duration.ofSeconds(120));
 
 		context.dispose();
-		context.onClose().block();
+		context.onDispose().block();
 
 		String body = response.receive().aggregate().asString(StandardCharsets.UTF_8).block();
 
@@ -230,7 +230,7 @@ public class HttpServerTests {
 				          .block(Duration.ofSeconds(120));
 
 		context.dispose();
-		context.onClose().block();
+		context.onDispose().block();
 
 		String body = response.receive().aggregate().asString(StandardCharsets.UTF_8).block();
 
@@ -255,7 +255,7 @@ public class HttpServerTests {
 		assertThat(response.status().code()).isEqualTo(200);
 		// dispose the Netty context and wait for the channel close
 		context.dispose();
-		context.onClose().block();
+		context.onDispose().block();
 
 		//REQUIRED - bug pool does not detect/translate properly lifecycle
 		HttpResources.reset();
@@ -269,7 +269,7 @@ public class HttpServerTests {
 		// fails, response status is 200 and debugging shows the the previous handler is called
 		assertThat(response.status().code()).isEqualTo(201);
 		context.dispose();
-		context.onClose().block();
+		context.onDispose().block();
 	}
 
 	@Test
